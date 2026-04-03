514b88b1a71f8c655861d00be571e84e229af247
Mouad Gourita <mouad.gourita@gmail.com>
2026-04-03 16:25:14 +0100

    feat: Implement SkillController methods for skill management and teacher skill association

--- app/Http/Controllers/SkillController.php
+++ app/Http/Controllers/SkillController.php
@@ -4,47 +4,52 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreSkillRequest;
 use App\Http\Requests\UpdateSkillRequest;
+use Illuminate\Support\Facades\DB;
 use App\Models\Skill;
+use App\Models\TeacherSkill;
+use Illuminate\Support\Facades\Auth;
 
 class SkillController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
-        //
-    }
+   public function index()
+   {
+      $skills = DB::table('users as u')
+               ->join('teacher_skills as ts', 'ts.users_id','=','u.id')
+               ->join('skills as s', 's.id', '=', 'ts.skills_id')
+               ->join('categories as c', 'c.id', '=', 's.categories_id')
+               ->select(
+                  'u.firstname',
+                  'u.lastname',
+                  'u.xp',
+                  's.*',
+                  'ts.level',
+                  'ts.years_experience as years',
+                  'c.category_name as category',
+                  'ts.id as teacherskillid',
+                  'u.id as teacherid'
+                  )->Where('s.is_active', True)
+                  ->get();
+      return View('Skills', compact('skills'));
+   }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(StoreSkillRequest $request)
-    {
-        //
-    }
+   public function create(StoreSkillRequest $request)
+   {
+      $validated = $request->validated();
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(Skill $skill)
-    {
-        //
-    }
+      $skill = Skill::create([
+         'name' => $validated['name'],
+         'description' => $validated['description'],
+         'icon_url' => $validated['icon_url'],
+         'categories_id' => $validated['categories_id'],
+      ]);
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(UpdateSkillRequest $request, Skill $skill)
-    {
-        //
-    }
+      TeacherSkill::Create([
+         'users_id' => Auth::user()->id,
+         'skills_id' => $skill->id,
+         'level' => $validated['level'],
+         'years_experience' => $validated['years'],
+      ]);
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Skill $skill)
-    {
-        //
-    }
+      return redirect('/teacher')->with('success','You Have Successsfully Added a New Skill');
+   }
 }
