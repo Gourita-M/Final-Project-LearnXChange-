fa56a4e28d07b477af492192ffb2cac158f76348
Mouad Gourita <mouad.gourita@gmail.com>
2026-04-13 11:49:07 +0100

    fix: Correctly filter active sessions by learner ID and tidy up query formatting

--- app/Http/Controllers/LearnerController.php
+++ app/Http/Controllers/LearnerController.php
@@ -12,21 +12,24 @@ class LearnerController extends Controller
     public function index()
     {
         $learner = auth::user();
+
         $totalsessions = Connect_Sessions::Where('learner_id', $learner->id)->count();
         $totalHours = DB::table('connect_sessions')
             ->where('learner_id', $learner->id)
             ->sum('duration');
+
         $ActiveSessions = DB::table('users as u')
             ->leftjoin('connect_sessions as cs', 'u.id', '=', 'cs.teacher_id')
             ->join('learning_requests as lr', 'lr.id', '=', 'cs.learning_requests_id')
             ->join('teacher_skills as ts', 'ts.id','=','lr.teacher_skills_id')
             ->join('skills as s','s.id','=','ts.skills_id')
             ->Where('cs.status', 'active')
+            ->Where('cs.learner_id', $learner->id)
             ->select(
                 'u.firstname','u.lastname','u.profilepic',
                 's.name',
                 'cs.duration', 'cs.session_type', 'cs.start_time'
-
+                ,'cs.id'
             )
             ->get();
 
