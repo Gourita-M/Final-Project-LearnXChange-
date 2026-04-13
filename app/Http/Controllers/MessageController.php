2357f2c91500c6f65cbe6f9cd7007f7575c2c68c
Mouad Gourita <mouad.gourita@gmail.com>
2026-04-13 11:49:18 +0100

    feat: Enhance MessageController with session validation and message retrieval

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -4,7 +4,12 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreMessageRequest;
 use App\Http\Requests\UpdateMessageRequest;
+use Illuminate\Support\Facades\Auth;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
 use App\Models\Message;
+use App\Events\MessageSent;
+use App\Models\Connect_sessions;
 use DateTime;
 
 class MessageController extends Controller
@@ -12,16 +17,42 @@ class MessageController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index($id)
     {
-        Return View('Messages.index');
+        $sessionid = $id;
+        $user = auth::user();
+        $connectSession = Connect_sessions::Where('id', $sessionid)
+                                ->Where('status', 'active')->first();
+        if($connectSession->learner_id === auth::user()->id || $connectSession->teacher_id === auth::user()->id){
+            return view('Messages.index', compact('sessionid'));
+        }
+        
+        if ($user->hasRole('teacher')) {
+            return redirect('/teacher');
+            }
+
+            if ($user->hasRole('learner')) {
+                return redirect('/learner');
+            }
     }
 
+    public function getAll($id)
+    {
+        $messages = Message::with('sender')
+            ->where('connect_sessions_id', $id)
+            ->orderBy('sent_at', 'asc')
+            ->get();
+
+        return response()->json([
+            'messages' => $messages
+        ]);
+    }
     /**
      * Store a newly created resource in storage.
      */
     public function store(StoreMessageRequest $request)
     {
+
         Message::Create([
             'content' => $request['content'],
             'is_read' => False,
@@ -29,30 +60,22 @@ class MessageController extends Controller
             'connect_sessions_id' => $request['connect_sessions_id'],
             'sender_id' => $request['sender_id'],
         ]);
+
         
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(Message $message)
+    public function send(Request $request)
     {
-        //
-    }
+        $message = Message::Create([
+            'content' => $request->message,
+            'is_read' => False,
+            'sent_at' => Carbon::now(), //i'll check it later wakha i don't need it 
+            'connect_sessions_id' => $request['connect_sessions_id'],
+            'sender_id' => auth::user()->id,
+        ]);
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(UpdateMessageRequest $request, Message $message)
-    {
-        //
-    }
+        //broadcast(new MessageSent($message))->toOthers();
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Message $message)
-    {
-        //
+        return response()->json($message);
     }
 }
