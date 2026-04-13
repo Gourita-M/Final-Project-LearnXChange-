9be7ce41c711e26c9ad362d911984791becb5e46
Mouad Gourita <mouad.gourita@gmail.com>
2026-04-13 11:49:24 +0100

    feat: Update ProfileController to include user skills and categories in profile view

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -4,16 +4,44 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 use App\Models\User;
+use App\Models\Categories;
 
 class ProfileController extends Controller
 {
     public function index()
     {
 
-        $user = auth()->user();
+        $user = auth::user();
 
-        return view('Profile.index', compact('user'));
+        $userskills = DB::table('users as u')
+                    ->join('teacher_skills as ts','ts.users_id','=','u.id')
+                    ->join('skills as s','s.id','=','ts.skills_id')
+                    ->select('s.name', 's.id as skillid')
+                    ->get();
+        $categories = Categories::get();
 
+        return view('Profile.index', compact('categories','user','userskills'));
+
+    }
+
+    public function editInfo(Request $request)
+    {
+        
+        $data = $request->validate([
+                'firstname' => 'required|string',
+                'lastname' => 'required|string',
+                'bio' => 'required|string',
+        ]);
+
+        User::Where('id', auth::user()->id)
+                ->update([
+                    'firstname' => $data['firstname'],
+                    'lastname' => $data['lastname'],
+                    'Bio' => $data['bio'],
+                ]);
+        
+        return Redirect('/profile')->with('success','Your Profile Info is Updated');
     }
 }
