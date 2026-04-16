d376ce463a39e2a9b2d322f4be65de0817b61483
Mouad Gourita <mouad.gourita@gmail.com>
2026-04-16 09:33:13 +0100

    feat: update AdminController to count pending certificates and refine certificate query

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -14,10 +14,22 @@ class AdminController extends Controller
     {
 
         $totalUsers = User::Count();
-        $totalCertificates = Certificates::Count();
+        $totalCertificates = Certificates::where('status', 'pending')->Count();
         $totalReports = Report::Count();
-        $certificates = DB::table('certificates')
-                        ->join('')
+        $certificates = DB::table('certificates as c')
+                        ->join('users as u','c.users_id','=','u.id')
+                        ->join('teacher_skills as ts','ts.users_id','=','u.id')
+                        ->join('skills as s','s.id','=','ts.skills_id')
+                        ->WhereColumn('s.id', 'c.skills_id')
+                        ->Where('status', 'pending')
+                        ->select(
+                            'c.created_at as date',
+                            's.name as skillname',
+                            'u.firstname',
+                            'u.lastname',
+                            'c.id as certifid'
+                            )
+                        ->get();
 
         return View('admin', compact('certificates','totalReports','totalCertificates','totalUsers'));
     }
